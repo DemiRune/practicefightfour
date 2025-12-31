b034bed2c4513d25d0914c12da43265efe0ffcf3
DemiRune <nikkisecho99@gmail.com>
2025-12-31 12:00:33 -0800

    lallaaa

--- practicefightfour/Program.cs
+++ practicefightfour/Program.cs
@@ -12,14 +12,50 @@ bool InventorySystem.CheckIfWeHaveItem( ItemType itemType, int Count);*/
 // methods to each item class (perform action? different perform methods based on item type)
 // devotion / blessing mechanic
 
+
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+public class CharacterBaseStats
+{
+    
+    public int Level { get; set; }
+    public int Experience { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Devotion { get; set; }
+
+    public CharacterBaseStats(int level, int experience, int attack, int defense, int devotion)
+    {
+        this.Level = level;
+        this.Experience = experience;
+        this.Attack = attack;
+        this.Defense = defense;
+        this.Devotion = devotion;
+    }
+}
 class SonPlayer
 {
-    public int Health { get; set; }
-    public int Defense { get; set; }
+    public CharacterBaseStats SonStats = new CharacterBaseStats(1, 0, 5, 5, 0);
+    public void AttackEnemy()
+    {
+       
+    }
+
+
+}
+
+class SonActions
+{
 
 }
 
 class EstherPlayer
+{
+
+}
+
+class EstherActions
 {
 
 }
@@ -35,12 +71,22 @@ class PanickingStatus // Chances to miss or not attack drastically raise, crits 
    
 }
 
-class RestrainedStatus // Unability to use action besides
+class RestrainedStatus // All actions fail. Take damage on each turn
 {
 
 }
 
-class BleedingStatus
+class BleedingStatus // Take damage on each turn. If untreated, adds Tired status, then develops into Exhausted.
+{
+
+}
+
+class TiredStatus // Slight drop on all stats. Praying grants less devotion.
+{
+
+}
+
+class ExhaustedStatus // Drastic drop on all stats. High chance to miss or will straight up not attack and spells may not work. Takes more damage.
 {
 
 }
@@ -60,5 +106,6 @@ class Program
 class TurnStart
 {
     // method check if status true <-- create for each actor
+    // method check end of battle if experience reaches requirement to level up
 }
 
